20e0d8c42f84b02caff89fa595f54a233faeafb2
Lucas Verri <lfverri@sga.pucminas.br>
2024-09-16 21:04:04 -0300

    Criação e configuração dos modelos de veiculo e consumo.

---
+++ mf-apis-web-services-fuel-manager/Models/Consumo.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        [Required]
+        public decimal Valor { get; set; }
+
+        [Required]
+        public tipoCombustivel Tipo { get; set; }
+
+        [Required]
+        public int VeiculoId { get; set; } //fk
+
+        public Veiculo Veiculo { get; set; } //navegacao
+        public enum tipoCombustivel
+        {
+            Diesel, //0
+            Etanol, //1
+            Gasolina //2
+        }
+    }
+}
