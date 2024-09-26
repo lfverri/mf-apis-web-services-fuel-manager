b35590d4d7507955272c2a46a4f5c0aa75df83bc
Lucas Verri <lfverri@sga.pucminas.br>
2024-09-25 22:38:35 -0300

    Criação do Controller Consumos.

--- mf-apis-web-services-fuel-manager/Controllers/ConsumosController.cs
+++ mf-apis-web-services-fuel-manager/Controllers/ConsumosController.cs
@@ -2,25 +2,24 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Cryptography.Xml;
 
 namespace mf_apis_web_services_fuel_manager.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class VeiculosController : ControllerBase
+    public class ConsumosController : ControllerBase
     {
         private readonly AppDbContext _context;
 
-        public VeiculosController(AppDbContext context)
+        public ConsumosController(AppDbContext context)
         {
-             _context = context;
+            _context = context;
         }
 
         [HttpGet] //get all para pegar todos os veiculos 
-        public async Task<ActionResult> GetAll() 
+        public async Task<ActionResult> GetAll()
         {
-            var model = await _context.Veiculos.ToListAsync();
+            var model = await _context.Consumos.ToListAsync();
 
             return Ok(model);
         }
@@ -28,8 +27,7 @@ namespace mf_apis_web_services_fuel_manager.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult> GetById(int id)
         {
-            var model = await _context.Veiculos
-                .Include(c => c.Consumos)
+            var model = await _context.Consumos
                 .FirstOrDefaultAsync(c => c.Id == id);
 
             if (model == null) return NotFound();
@@ -37,54 +35,45 @@ namespace mf_apis_web_services_fuel_manager.Controllers
             return Ok(model);
         }
 
-
         [HttpPost]
-        public async Task<ActionResult> Create(Veiculo model)
+        public async Task<ActionResult> Create(Consumo model)
         {
-            if (model.AnoFabricacao <= 0 || model.AnoModelo <= 0)            
-            {
-                return BadRequest(new { message = "Os campos Ano de Fabricação e Ano do Modelo não foram preenchidos corretamente." });
-
-            }
-
-            _context.Veiculos.Add(model);
+            _context.Consumos.Add(model);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetById", new {id = model.Id}, model);
+            return CreatedAtAction("GetById", new { id = model.Id }, model);
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult> Update(int id, Veiculo model)
+        public async Task<ActionResult> Update(int id, Consumo model)
         {
             if (id != model.Id) return BadRequest();
 
-            var modeloDb = await _context.Veiculos.AsNoTracking()
+            var modeloDb = await _context.Consumos.AsNoTracking()
                 .FirstOrDefaultAsync(c => c.Id == id);
 
             if (modeloDb == null) return NotFound();
 
 
-            _context.Veiculos.Update(model);
+            _context.Consumos.Update(model);
 
             await _context.SaveChangesAsync();
 
             return NoContent();
 
         }
-
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> Delete(int id)
         {
-         var model = await _context.Veiculos.FindAsync(id);
-            if (model == null)  return NotFound();
+            var model = await _context.Consumos.FindAsync(id);
+            if (model == null) return NotFound();
 
-            _context.Veiculos.Remove(model);
+            _context.Consumos.Remove(model);
 
             await _context.SaveChangesAsync();
 
             return NoContent();
         }
-
     }
 }
--- mf-apis-web-services-fuel-manager/Program.cs
+++ mf-apis-web-services-fuel-manager/Program.cs
@@ -1,5 +1,7 @@
 using mf_apis_web_services_fuel_manager.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Text.Json.Serialization;
 namespace mf_apis_web_services_fuel_manager
 {
     public class Program
@@ -10,7 +12,9 @@ namespace mf_apis_web_services_fuel_manager
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+                
             builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             
