6749b087541bb886e10d1e21013e63a37aaf0a3c
Lucas Verri <lfverri@sga.pucminas.br>
2024-09-25 22:12:36 -0300

    Criação controller Veiculos

---
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -0,0 +1,89 @@
+﻿using mf_apis_web_services_fuel_manager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.Xml;
+
+namespace mf_apis_web_services_fuel_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public VeiculosController(AppDbContext context)
+        {
+             _context = context;
+        }
+
+        [HttpGet] //get all para pegar todos os veiculos 
+        public async Task<ActionResult> GetAll() 
+        {
+            var model = await _context.Veiculos.ToListAsync();
+
+            return Ok(model);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Veiculos
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (model == null) return NotFound();
+
+            return Ok(model);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Veiculo model)
+        {
+            if (model.AnoFabricacao <= 0 || model.AnoModelo <= 0)            
+            {
+                return BadRequest(new { message = "Os campos Ano de Fabricação e Ano do Modelo não foram preenchidos corretamente." });
+
+            }
+
+            _context.Veiculos.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new {id = model.Id}, model);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Veiculo model)
+        {
+            if (id != model.Id) return BadRequest();
+
+            var modeloDb = await _context.Veiculos.AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (modeloDb == null) return NotFound();
+
+
+            _context.Veiculos.Update(model);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+         var model = await _context.Veiculos.FindAsync(id);
+            if (model == null)  return NotFound();
+
+            _context.Veiculos.Remove(model);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
--- mf-apis-web-services-fuel-manager/Models/Veiculo.cs
+++ mf-apis-web-services-fuel-manager/Models/Veiculo.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace mf_apis_web_services_fuel_manager.Models
 {
